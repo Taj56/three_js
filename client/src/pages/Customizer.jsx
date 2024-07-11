4f8e81607e572f43c95ba8ed36cd80c4d44ac09f
Taj56 <64923628+Taj56@users.noreply.github.com>
2024-07-11 13:11:46 -0500

    home page initial

---
+++ client/src/pages/Customizer.jsx
@@ -0,0 +1,31 @@
+import { useState, useEffect } from "react";
+import { AnimatePresence, motion } from "framer-motion";
+import { useSnapshot } from "valtio";
+
+import config from '../config/config';
+import state from "../store";
+import { download } from "../assets";
+
+import { downloadCanvasToImage, reader } from "../config/helpers";
+import {EditorTabs, FilterTabs} from "../config/constants";
+import { fadeAnimation, slideAnimation } from "../config/motion";
+
+import { CustomButton, AIPicker, ColorPicker, FilePicker, Tab } from "../components";
+
+
+const Customizer = () => {
+
+  const snap = useSnapshot(state);
+
+  return (
+    <AnimatePresence>
+      {!snap.intro && (
+        <>
+          Customizer
+        </>
+      )}
+    </AnimatePresence>
+  )
+}
+
+export default Customizer
