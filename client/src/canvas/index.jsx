145c37fd0821d8f506648fbfb7983674a7748612
Taj56 <64923628+Taj56@users.noreply.github.com>
2024-07-13 09:59:29 -0500

    components setup
    bug located in the index.jsx canvas

--- client/src/canvas/index.jsx
+++ client/src/canvas/index.jsx
@@ -1,7 +1,38 @@
-const Canvas = () => {
+
+import { Canvas } from "@react-three/fiber";
+import {Environment, Center} from "@react-three/drei";
+
+import Shirt from './Shirt';
+import CameraRig from './CameraRig';
+import Backdrop from './Backdrop';
+
+const CanvasModel = () => {
   return (
-    <div>Canvas</div>
+
+    <Canvas>
+        <ambientLight />
+        {/* <Environment preset="city"/> */}
+
+        <CameraRig>
+          {/* <Backdrop/> */}
+          <Center>
+            <Shirt />
+          </Center>
+        </CameraRig>
+    </Canvas>
+
+    // <Canvas>
+    //   <ambientLight intensity={0.5} />
+    //   <Environment preset="city" />
+
+    //   <CameraRig>
+    //     {/* <Backdrop /> */}
+    //     <Center>
+    //       <Shirt />
+    //     </Center>
+    //   </CameraRig>
+    // </Canvas>
   )
 }
 
-export default Canvas
+export default CanvasModel
